62b024bfe4f39c061b6f92a9425c617bd133d3f9
Rhuan Carvalho <carvalho.rhuan@gmail.com>
2023-06-25 14:18:19 -0300

    atualização imagens

--- src/js/index.js
+++ src/js/index.js
@@ -5,9 +5,9 @@ botaoAlterarTema.addEventListener("click", function () {
   body.classList.toggle("modo-escuro");
   const imagemBotao = botaoAlterarTema.querySelector("img");
   if (body.classList.contains("modo-escuro")) {
-    imagemBotao.src = "./SRC/Imagens/moon.png";
+    imagemBotao.src = "./src/imagens/moon.png";
   } else {
-    imagemBotao.src = "./SRC/Imagens/sun.png";
+    imagemBotao.src = "./src/imagens/sun.png";
   }
 });
 const imagemInterativa = document.querySelector("#cartaoespecial .face.front img");
